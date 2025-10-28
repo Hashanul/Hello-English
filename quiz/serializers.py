0befaaa7a1d4fe62b9ecb24b03897b237b229449
Hashanul <hashanulbanna4492@gmail.com>
2025-10-28 16:42:16 +0600

    added BannerSerializers, BannerViewSet and Banner urls.

---
+++ quiz/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Banner
+
+
+class BannerSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Banner
+        fields = '__all__'
+
+    
+
