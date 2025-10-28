0d8ada6a4078f11a85f493a902c231cf6d0e0e56
Hashanul <hashanulbanna4492@gmail.com>
2025-10-28 16:28:17 +0600

    added a Banner model, and register Banner in admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Banner
 
-# Register your models here.
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'is_active', 'created_at']
+    list_filter = ['is_active']
+    search_fields = ['title']
+    ordering = ['-created_at']
+
+    
+
+
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
+    button_link = models.CharField(max_length=100, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Banner : {self.title}"
+
+
 
 
